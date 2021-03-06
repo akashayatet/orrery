aa3b3969f5b72f383a61ef3eb7255bae2cf3347f
akashayatet <akashayatet@logaeth.org>
2021-03-06 12:50:06 -0800

    incorporated waveforms

--- scripts/mind.js
+++ scripts/mind.js
@@ -3,7 +3,7 @@ window.addEventListener('DOMContentLoaded', () => {
     // Constants
     const RAD = (fromDegrees) => { return BABYLON.Tools.ToRadians(fromDegrees) } /* Verbosity bump */
     const A_ALIAS = 1       /* Anti-aliasing Preference */
-    const COMPLEXITY = 12   /* Tesselations of the Torai and Segments of the Sphera */
+    const COMPLEXITY = 27   /* Tesselations of the Torai and Segments of the Sphera */
     const SUPERPOS = 1      /* Y Axis Superposition value */
     const DIA_A = 2         /* Alpha Diameter */
     const DOPPLER = 0.0    /* Anaglyph Red/Blue shift intensity */
@@ -54,7 +54,7 @@ window.addEventListener('DOMContentLoaded', () => {
     const TOREUS = (diame, thicc, tessells) => {
         const TOREUS_GEO = {
             diameter: DIA_A + (diame ? diame : 0),
-            thickness: (RAD(DIA_A)*2) + (thicc ? thicc : 0),    /* Should equally match Rayarc bandwidths */
+            thickness: (RAD(DIA_A)*12) + (thicc ? thicc : 0),    /* Should equally match Rayarc bandwidths */
             tessellation: COMPLEXITY + (tessells ? tessells : 0)/* Tesselations will also affect color depth */
         }
         return TOREUS_GEO
@@ -84,8 +84,17 @@ window.addEventListener('DOMContentLoaded', () => {
     const subFactor3 = 0.03
     const subFactor6 = 0.06
     const subFactor9 = 0.09
-    // Waveform
-    let biowave = 0
+    // Waveforms
+    const rateOfChange = 0.042
+    const waveForms = [
+        {current: 0.02, limit: 2, peaked: false},
+        {current: 0.04, limit: 3, peaked: false},
+        {current: 0.06, limit: 4, peaked: false},
+        {current: 0.08, limit: 5, peaked: false},
+        {current: 0.10, limit: 6, peaked: false},
+        {current: 0.12, limit: 7, peaked: false},
+        {current: 0.16, limit: 8, peaked: false}
+    ]
 
     // Generate scene
     const createScene = () => {
@@ -106,8 +115,8 @@ window.addEventListener('DOMContentLoaded', () => {
          */
         
         // Akali Atet Toreus - Black / Death
-        const nToreus = BABYLON.MeshBuilder.CreateTorus('nToreus', TOREUS(), scene)
-        nToreus.position.y = SUPERPOS
+        const nToreus = BABYLON.MeshBuilder.CreateTorus('nToreus', TOREUS(waveForms[0].current), scene)
+        nToreus.position.y = SUPERPOS+0
         nToreus.position.x = SINGULARITY
         nToreus.rotation = new BABYLON.Vector3(
             alphaRot*subFactor3, 
@@ -120,8 +129,8 @@ window.addEventListener('DOMContentLoaded', () => {
         nToreus.material = toreusBlack // n is for nULL
 
         // Akosh Atet Toreus - White / Life
-        const gToreus = BABYLON.MeshBuilder.CreateTorus('gToreus', TOREUS(biowave), scene)
-        gToreus.position.y = SUPERPOS
+        const gToreus = BABYLON.MeshBuilder.CreateTorus('gToreus', TOREUS(waveForms[1].current), scene)
+        gToreus.position.y = SUPERPOS+1
         gToreus.position.x = SINGULARITY
         gToreus.rotation = new BABYLON.Vector3(
             alphaRot*subFactor9, 
@@ -134,8 +143,8 @@ window.addEventListener('DOMContentLoaded', () => {
         gToreus.material = toreusWhite // g is for gAIA
 
         // Aura Atet Toreus - Yellow / Air / Subliminal
-        const mToreus = BABYLON.MeshBuilder.CreateTorus('mToreus', TOREUS(), scene)
-        mToreus.position.y = SUPERPOS
+        const mToreus = BABYLON.MeshBuilder.CreateTorus('mToreus', TOREUS(waveForms[2].current), scene)
+        mToreus.position.y = SUPERPOS-1
         mToreus.position.x = SINGULARITY
         mToreus.rotation = new BABYLON.Vector3(
             alphaRot*subFactor3, 
@@ -148,8 +157,8 @@ window.addEventListener('DOMContentLoaded', () => {
         mToreus.material = toreusYellow // m is for mAIA
 
         // Cryo Atet Toreus - Blue / Water / Darkness
-        const aToreus = BABYLON.MeshBuilder.CreateTorus('aToreus', TOREUS(), scene)
-        aToreus.position.y = SUPERPOS
+        const aToreus = BABYLON.MeshBuilder.CreateTorus('aToreus', TOREUS(waveForms[3].current), scene)
+        aToreus.position.y = SUPERPOS-2
         aToreus.position.x = SINGULARITY-(DOPPLER+DOPPLER)
         aToreus.rotation = new BABYLON.Vector3(
             alphaRot*subFactor6, 
@@ -162,8 +171,8 @@ window.addEventListener('DOMContentLoaded', () => {
         aToreus.material = toreusBlue // a is for aBYSS
 
         // Zero Atet Toreus - Teal / Vaccuum / Plenum
-        const zToreus = BABYLON.MeshBuilder.CreateTorus('cToreus', TOREUS(), scene)
-        zToreus.position.y = SUPERPOS
+        const zToreus = BABYLON.MeshBuilder.CreateTorus('cToreus', TOREUS(waveForms[4].current), scene)
+        zToreus.position.y = SUPERPOS-3
         zToreus.position.x = SINGULARITY-DOPPLER
         zToreus.rotation = new BABYLON.Vector3(
             alphaRot*subFactor9, 
@@ -176,8 +185,8 @@ window.addEventListener('DOMContentLoaded', () => {
         zToreus.material = toreusTeal // z is for zERO-POINT
 
         // Pyro Atet Toreus - Magenta / Fire / Light
-        const pToreus = BABYLON.MeshBuilder.CreateTorus('pToreus', TOREUS(), scene)
-        pToreus.position.y = SUPERPOS
+        const pToreus = BABYLON.MeshBuilder.CreateTorus('pToreus', TOREUS(waveForms[5].current), scene)
+        pToreus.position.y = SUPERPOS-4
         pToreus.position.x = SINGULARITY+DOPPLER
         pToreus.rotation = new BABYLON.Vector3(alphaRot*subFactor9, betaRot*subFactor3, gammaRot*subFactor6)
         const toreusMagenta = new BABYLON.StandardMaterial(scene)
@@ -186,8 +195,8 @@ window.addEventListener('DOMContentLoaded', () => {
         pToreus.material = toreusMagenta // p is for pLASMA
 
         // Zon Atet Toreus - Purple / Chaos / Transmutation
-        const oToreus = BABYLON.MeshBuilder.CreateTorus('oToreus', TOREUS(), scene)
-        oToreus.position.y = SUPERPOS
+        const oToreus = BABYLON.MeshBuilder.CreateTorus('oToreus', TOREUS(waveForms[6].current), scene)
+        oToreus.position.y = SUPERPOS-5
         oToreus.position.x = SINGULARITY+(DOPPLER+DOPPLER)
         oToreus.rotation = new BABYLON.Vector3(alphaRot*subFactor6, betaRot*subFactor3, gammaRot*subFactor9)
         const toreusPurple = new BABYLON.StandardMaterial(scene)
@@ -223,28 +232,26 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     
     // Experiments...
-    let limitReached = false
 
     engine.runRenderLoop(() => {
         // --- Transmutations --- \\
         const ROTVAL = Math.PI
         const ENTROPY = 1.618
         // alphaRot -= (ROTVAL / ENTROPY)
-        // betaRot -= (ROTVAL / ENTROPY)
-        // gammaRot -= (ROTVAL / ENTROPY)
+        betaRot -= (ROTVAL / ENTROPY)
+        gammaRot -= (ROTVAL / ENTROPY)
 
         // Expansion|Contraction
-        let rate = 0.25
-        const source = 00
-        const limit = 6
-
-        if (biowave != limit && !limitReached) {
-            biowave += rate
-        } else if (limitReached && biowave > 00) {
-            biowave -= rate
-        } else {
-            limitReached = biowave != 00
-        }
+        waveForms.forEach(wave => {
+            console.debug(wave)
+            if (!(wave.current >= wave.limit) && !wave.peaked) {
+                wave.current += rateOfChange
+            } else if (wave.peaked && wave.current > 00) {
+                wave.current -= rateOfChange
+            } else {
+                wave.peaked = !(wave.current <= 00)
+            }
+        })
 
         
         // Iterate Scene
